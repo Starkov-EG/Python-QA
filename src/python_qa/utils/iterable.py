dc00d640c24e6cd96afd229542d510a7fba3f237
E.Starkov <starkov.ev.ge@gmail.com>
2023-09-28 20:03:11 +0300

    fix

--- src/python_qa/utils/iterable.py
+++ src/python_qa/utils/iterable.py
@@ -5,13 +5,14 @@ def filtered(func: Callable, iterable: Iterable):
     return type(iterable)(filter(func, iterable))
 
 
-def select_items(items: List[dict], inverse: bool = False, **kwargs) -> List:
+def select_items(items: List[dict | object], inverse: bool = False, **kwargs) -> List:
     res = []
     if kwargs:
         for item in items:
             selected = True
             for k, v in kwargs.items():
-                if (item.get(k) != v and not inverse) or (item.get(k) == v and inverse):
+                item_v = item.get(k) if hasattr(item, "get") else getattr(item, k, None)
+                if (item_v != v and not inverse) or (item_v == v and inverse):
                     selected = False
                     break
             if selected:
@@ -20,7 +21,7 @@ def select_items(items: List[dict], inverse: bool = False, **kwargs) -> List:
 
 
 def select_item(items: List[dict], inverse: bool = False, **kwargs) -> dict:
-    res = select_items(items, inverse, kwargs)
+    res = select_items(items, inverse, **kwargs)
     if res:
         return res[0]
     return {}
